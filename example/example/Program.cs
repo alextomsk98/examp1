00870037a37d2ff45467e13fbfa34c54c3c20217
alextomsk98 <shavlo.2016@mail.ru>
2018-02-05 17:44:31 +0700

    05.02.18
    laba1(1)

---
+++ example/example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Class Types ******\n");
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+            for (int i = 0; i <= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
